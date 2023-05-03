29fe0b470d2a8a350631c1b8630b10c83cdd7517
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-03 23:44:33 +0530

    [Sachin] Adding Pattern1

--- OopsPracticeProblems/Program.cs
+++ OopsPracticeProblems/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the oops concept practice problems");
+            Console.WriteLine("Select the program given below");
+            Console.WriteLine("\n1:Pattern");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    PrintPattern.PatternFunction();
+                    break;
+            }
         }
     }
 }
