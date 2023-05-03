22f440dfb4f743078c85dccaded03bdf230c5d85
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-03 23:31:24 +0530

    [Sachin] Adding Welcome Message

---
+++ OopsPracticeProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OopsPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the oops concept practice problems");
+        }
+    }
+}
