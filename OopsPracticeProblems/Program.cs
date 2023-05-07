e197b693c7eff34657407cdbf5d7108c3fe81648
Sachinmahajana <sachinmahajan782@gmail.com>
2023-05-07 11:22:34 +0530

    [Sachin] Adding FindMaxMinArray

--- OopsPracticeProblems/Program.cs
+++ OopsPracticeProblems/Program.cs
@@ -6,12 +6,16 @@
         {
             Console.WriteLine("Welcome to the oops concept practice problems");
             Console.WriteLine("Select the program given below");
-            Console.WriteLine("\n1:Pattern");
+            Console.WriteLine("\n1:Pattern\n2:FindmaxminArray");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     PrintPattern.PatternFunction();
+                    break;
+                case 2:
+                    Findmaxmin myObj = new Findmaxmin();
+                    myObj.FindMaxMinFunction();
                     break;
             }
         }
